2f272bb68d7349b09a9917d6a6c1f859f6993674
Ssr1997 <64249849+Ssr1997@users.noreply.github.com>
2024-08-12 17:06:05 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,34 @@
+"use strict" // treats all JS code as newer version
+
+// alert (3 + 3) // We are using NodeJS and not browser console
+
+console.log( 3 
+    +
+    3)      // Code Readability should be high.
+
+console.log("Shivam");
+
+let name = "Neha"
+let age = 28
+let isLoggedIn = false
+let city;
+let state = null
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value ,in short it's a representaiton of empty value.
+// undefined 
+// symbol => unique
+
+// The typeof() operator in JavaScript is used to check the data type of a variable or value.
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(undefined));   // undefined
+console.log(typeof(null));        // object
+
+
+
