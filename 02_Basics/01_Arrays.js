0be620a14de6082a53993ea936c939a283319d36
Ssr1997 <64249849+Ssr1997@users.noreply.github.com>
2024-08-15 18:53:40 +0000

    Arrays

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,18 @@
+/* An array is a linear data structure that holds a collection of elements, all of the same data type, 
+arranged in a sequential manner. 
+Each element in an array is identified by an index or a key, allowing for efficient access and modification of data.
+Arrays can contain elements of various data types, including numbers, strings, objects, and even other arrays. 
+
+Dynamic Sizing: Arrays in JavaScript are dynamic, meaning their size can be adjusted at runtime.
+Index-Based Access: Elements are accessed using numerical indices, starting from 0.
+Flexible Element Types: Arrays can hold different types of elements simultaneously, such as numbers and strings.
+*/
+
+const arr1 = [5,8,3,7,9,4]
+console.log(arr1);
+console.log("Element at 4rth index is : ",arr1[4]);
+console.log("Length of arr1 is :", arr1.length);
+
+const arr2 = new Array(3,8,4,3,9)
+console.log(arr2);
+
