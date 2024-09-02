81cc09cfd9ca71a7fbdbd87852e247324aec3a97
Ssr1997 <64249849+Ssr1997@users.noreply.github.com>
2024-09-02 15:19:14 +0000

    Control Flow

---
+++ 04_Control_Flow/Switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "Sept"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "june":
+        console.log("June");
+        break;
+    case "July":
+        console.log("July");
+        break; 
+    default:
+        console.log("Default case match");
+        break;
+}
