d6e18f05c8ebbbce4817626ddb6cae62b4c4479c
Ssr1997 <64249849+Ssr1997@users.noreply.github.com>
2024-09-04 14:44:44 +0000

    Loops

--- 05_Loops_Iterations/ForIn_Loop.js
+++ 05_Loops_Iterations/ForIn_Loop.js
@@ -1,15 +1,26 @@
+// Define an object with key-value pairs where the keys are abbreviations and the values are programming languages
 const myObj = {
-    js:"JavaScript",
-    rb:"Ruby",
-    cj:"Clojure",
-    go:"Go"
-}
+    js: "JavaScript",  // "js" is the abbreviation for "JavaScript"
+    rb: "Ruby",        // "rb" is the abbreviation for "Ruby"
+    cj: "Clojure",     // "cj" is the abbreviation for "Clojure"
+    go: "Go"           // "go" is the abbreviation for "Go"
+};
 
-for(const key in myObj){
+// Use a for...in loop to iterate over the keys of the object
+for (const key in myObj) {
+    // For each key in the object, log a string that shows the key and its corresponding value
     console.log(`${key} is shortcut for ${myObj[key]}`);
+    // Example output: "js is shortcut for JavaScript"
 }
 
-const programming = ["js","rb","c++","clojure"]
-for(const key in programming){
+/////////////////////////////////////
+
+// Define an array with elements representing programming language abbreviations
+const programming = ["js", "rb", "c++", "clojure"];
+
+// Use a for...in loop to iterate over the indices of the array
+for (const key in programming) {
+    // Log the element at the current index (key) to the console
     console.log(programming[key]);
+    // Example output: "js", "rb", "c++", "clojure"
 }
