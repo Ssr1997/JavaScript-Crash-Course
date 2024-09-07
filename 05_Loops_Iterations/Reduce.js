10090928ef18ffcb1ad3bae82d6a58cc73225e0a
Ssr1997 <64249849+Ssr1997@users.noreply.github.com>
2024-09-07 08:28:00 +0000

    Loops

--- 05_Loops_Iterations/Reduce.js
+++ 05_Loops_Iterations/Reduce.js
@@ -1,26 +1,29 @@
+// An array of numbers
 const myNums = [1, 2, 3]
 
-const myTotal = myNums.reduce(function(acc,curval){
+// Using reduce with a function to accumulate values
+const myTotal = myNums.reduce(function(acc, curval) {
+    // Logging the current state of the accumulator and the current value
     console.log(`accumulator : ${acc} & currentvalue : ${curval}`);
     
+    // Accumulating the sum by adding the current value to the accumulator
     return acc + curval
-},69)
-console.log(myTotal);
+}, 69) // Initial value of the accumulator is set to 69
+console.log(myTotal); // Logs the final accumulated value
 
-const myTotal2 = myNums.reduce((acc,curval) => acc+curval,0)
-console.log(myTotal2);
+// Alternative syntax: Using an arrow function for the reduce operation
+const myTotal2 = myNums.reduce((acc, curval) => acc + curval, 0)
+// Initial value of the accumulator is set to 0
+console.log(myTotal2); // Logs the final accumulated value
 
+// An array of objects representing items in a shopping cart
 const shoppingCart = [
-    { itemName: "js course",
-      price: "999"
-    },
-    { itemName: "python",
-      price: "2999"
-    },
-    { itemName: "data science",
-      price: "5999"
-    }
+    { itemName: "js course", price: 999 },
+    { itemName: "python", price: 2999 },
+    { itemName: "data science", price: 5999 }
 ]
 
-const priceToPay = shoppingCart.reduce((acc,item) => acc+item.price,0)
-console.log(priceToPay);
+// Using reduce to calculate the total price of all items in the cart
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+// Initial value of the accumulator is set to 0
+console.log(priceToPay); // Logs the total price of all items
